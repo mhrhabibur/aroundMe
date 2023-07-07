353e27367afa6605232c1d0457dd686311d23836
Habibur Rahman <mhr.habibur@gmail.com>
2023-07-07 23:15:30 +0600

    add a test location

--- AroundMe/MapViewController.swift
+++ AroundMe/MapViewController.swift
@@ -4,17 +4,23 @@
 //
 //  Created by Habibur Rahman on 7/7/23.
 //
-
+import MapKit
 import UIKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet var mapView: MKMapView!
+    let location = CLLocation(latitude: 28.613939, longitude: 77.209023)
+    let region_radious = 10000
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        centerMap(location: location)
     }
     
-
+    func centerMap(location: CLLocation) {
+        let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: CLLocationDistance(region_radious), longitudinalMeters: CLLocationDistance(region_radious))
+        mapView.setRegion(region, animated: true)
+    }
 
 }
